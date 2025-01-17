206f2e8257395186f44fe8f4fe3ae03741185197
dmitryiordanov <dmitryiordanov@gmail.com>
2025-01-18 00:09:41 +0200

    Add Logo image and Animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-alert('Script is work!');
+const image = document.querySelector('.image');
+
+function getRandomSize() {
+    return Math.floor(Math.random() * 200) + 150;
+}
+
+image.addEventListener('click', () => {
+    image.style.width = `${getRandomSize()}px`;
+    image.style.height = `${getRandomSize()}px`;
+});
